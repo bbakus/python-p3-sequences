5b21f41fdd64ce8fb90bf14b0a3ca4577edd7511
Brandon Bakus <brandonbakus@gmail.com>
2025-02-18 14:52:42 -0500

    yeah this was hard

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    sequence = [0, 1]
+    
+    for i in range(2, length):
+        sequence.append(sequence[i-1] + sequence[i-2])
+        
+    print(sequence[:length])  
